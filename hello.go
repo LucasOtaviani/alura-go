1d5a80dd456776e97ff0c17fe52f30f49ee60ac0
Lucas Otaviani <lucas.otaviani@hotmail.com>
2022-02-23 19:57:41 -0300

    Understanding how variable works

--- hello.go
+++ hello.go
@@ -1,7 +1,28 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
+	name := "Lucas"
+	age := 20
+	version := 1.17
+
+	/* Works calling 'var' */
+	// var name string = "Lucas"
+	// var age int = 20
+	// var version float32 = 1.17
+
+	/* Works without a type */
+	// var name = "Lucas"
+	// var age = 20
+	// var version = 1.17
+
 	fmt.Println("Hello World!")
+	fmt.Println("Hello, Mr.", name, "your age is", age)
+	fmt.Println("This program is running on version", version)
+
+	fmt.Println("Type of name variable is:", reflect.TypeOf(name))
+	fmt.Println("Type of name variable is:", reflect.TypeOf(age))
+	fmt.Println("Type of name variable is:", reflect.TypeOf(version))
 }
