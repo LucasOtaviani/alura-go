9e0225f7fd4c9ad2c164c7cd704f3bbc1b8ebe98
Lucas Otaviani <lucas.otaviani@hotmail.com>
2022-02-23 20:06:31 -0300

    Reading input

--- hello.go
+++ hello.go
@@ -1,28 +1,22 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
 	name := "Lucas"
-	age := 20
 	version := 1.17
 
-	/* Works calling 'var' */
-	// var name string = "Lucas"
-	// var age int = 20
-	// var version float32 = 1.17
-
-	/* Works without a type */
-	// var name = "Lucas"
-	// var age = 20
-	// var version = 1.17
-
-	fmt.Println("Hello World!")
-	fmt.Println("Hello, Mr.", name, "your age is", age)
+	fmt.Println("Hello, Mr.", name)
 	fmt.Println("This program is running on version", version)
 
-	fmt.Println("Type of name variable is:", reflect.TypeOf(name))
-	fmt.Println("Type of name variable is:", reflect.TypeOf(age))
-	fmt.Println("Type of name variable is:", reflect.TypeOf(version))
+	fmt.Println("1. Iniciar Monitoramento")
+	fmt.Println("2. Exibir Logs")
+	fmt.Println("0. Sair do Programa")
+
+	var command int
+	
+	// fmt.Scanf("%d", &command)
+
+	fmt.Scan(&command)
+	fmt.Println(command)
 }
