4ea1da1ef7cff0859255e43d7a706be858c91abe
Lucas Otaviani <lucas.otaviani@hotmail.com>
2022-02-25 10:21:50 -0300

    Extracted functions

--- hello.go
+++ hello.go
@@ -1,21 +1,13 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	name := "Lucas"
-	version := 1.17
+	showIntroduction()
+	showMenu()
 
-	fmt.Println("Hello, Mr.", name)
-	fmt.Println("This program is running on version", version)
-
-	fmt.Println("1. Iniciar Monitoramento")
-	fmt.Println("2. Exibir Logs")
-	fmt.Println("0. Sair do Programa")
-
-	var command int
-
-	fmt.Scan(&command)
+	command := readCommand()
 
 	switch command {
 	case 1:
@@ -24,7 +16,30 @@ func main() {
 		fmt.Println("Wow!")
 	case 0:
 		fmt.Println("Dying! :)")
+		os.Exit(0)
 	default:
 		fmt.Println("Sorry, I'm a robot and I don't know this expression :(")
+		os.Exit(-1)
 	}
+}
+
+func showIntroduction() {
+	name := "Lucas"
+	version := 1.17
+
+	fmt.Println("Hello, Mr.", name)
+	fmt.Println("This program is running on version", version)
+}
+
+func showMenu() {
+	fmt.Println("1. Iniciar Monitoramento")
+	fmt.Println("2. Exibir Logs")
+	fmt.Println("0. Sair do Programa")
+}
+
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+
+	return command
 }
